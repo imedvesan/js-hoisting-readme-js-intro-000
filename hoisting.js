fdb70be8c4414786522502c94255ddb09014bf0e
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-01-28 21:41:36 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,19 +1,20 @@
 function callMe() {
+var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+thisIsCrazy();
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
+
   }
 }
 
@@ -21,7 +22,7 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+  sayMy()();
 
   function sayMy() {
     console.log(name);
